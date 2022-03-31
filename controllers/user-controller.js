784dd4bf22135a3a26ab321e75f7edba81617288
TomFoolery <christophertravisp91@gmail.com>
2022-03-31 16:41:59 -0400

    fixed typo

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -25,7 +25,6 @@ getUserById({ params }, res) {
     })
     .select("-__v")
     .then((dbUserData) => {
-        // If no user is found
         if (!dbUserData) {
             res.status(404).json({ message: "No user found with this ID" });
             return;
@@ -37,7 +36,7 @@ getUserById({ params }, res) {
         res.status(400).json(err);
     });
 },
-createUser({ body }), res) {
+createUser({ body }, res) {
     User.create(body)
     .then((dbUserData) => res.json(dbUserData))
     .catch((err) => res.status(400).json(err));
