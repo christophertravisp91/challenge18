94081b6304a109345fb0af9856813d84d7e0a024
TomFoolery <christophertravisp91@gmail.com>
2022-04-02 12:21:53 -0400

    fixed routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,14 +6,10 @@ const {
     createUser,
     updateUser,
     deleteUser,
-    addFriend,
-    removeFriend,
 } = require("../../controllers/user-controller");
 
 router.route("/").get(getAllUsers).post(createUser);
 
 router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
-
-router.route("/:id/friends/:friendsId").post(addFriend).delete(removeFriend);
 
 module.exports = router;
